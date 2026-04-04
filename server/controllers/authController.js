cd6732e5a78b32acf86d1f91c8a120c122d0f237
Aziel Randel Rabano <imazielr@gmail.com>
2026-04-04 17:42:34 +0800

    set up back end. server/ index, auth, authcontroller, package.json

---
+++ server/controllers/authController.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+// temp DB for test only
+const user = {
+    username: "admin",
+    password: bcrypt.hashSync("1234", 10),
+};
+
+export const login = async (req, res) => {
+    const SECRET = process.env.JWT_SECRET;
+    console.log("SECRET:", SECRET);
+
+    try {
+        const {username, password} = req.body;
+
+        // 1. check username
+        if (username !== user.username) {
+            return res.status(401).json({message: "Invalid username"});
+        }
+
+        // 2. check password
+        const isValid = await bcrypt.compare(password, user.password);
+
+        if(!isValid) {
+            return res.status(401).json({message: "Wrong password"});
+        }
+
+        // 3. generate token
+        const token = jwt.sign({ username }, SECRET, { expiresIn: "1h"});
+
+        // 4. send response
+        res.json({ token });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: "Server error"});
+    }
+    
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running");

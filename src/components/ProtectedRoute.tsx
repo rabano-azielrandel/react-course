32bd61cd3c578445383116893c3c2ad5ccbe586c
Aziel Randel Rabano <imazielr@gmail.com>
2026-04-05 11:31:20 +0800

    use proper type in ProtectedRoute

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,7 +1,12 @@
 import { Navigate } from "react-router-dom";
 import { CONTAINS_TOKEN } from "@/lib/utils";
+import type { ReactNode } from "react";
 
-export default function ProtectedRoute({ children }: any) {
+type Props = {
+  children: ReactNode;
+};
+
+export default function ProtectedRoute({ children }: Props) {
   console.log("token:", CONTAINS_TOKEN());
 
   if (!CONTAINS_TOKEN()) {
